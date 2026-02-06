76fb26322d3f8f9828be21f58e36a03571e42b6c
addiiiti <aditiawez@gmail.com>
2026-02-06 16:20:41 +0000

    added:created an express server with the skeleton endpoints for the app

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()//creates an instance of express app
+const port = 3000
+
+app.post("user/signup",(req,res)=>{
+  res.json(
+    {
+      message: "signup endpoint"
+    }
+  )
+})
+
+app.post("user/signin",(req,res)=>{
+  res.json(
+    {
+      message: "signin endpoint"
+    }
+  )
+})
+app.post("user/purchases",(req,res)=>{
+  res.json(
+    {
+      message: "purchases endpoint"
+    }
+  )
+})
+app.post("user/purchase",(req,res)=>{
+  res.json(
+    {
+      message: "purchase endpoint"
+    }
+  )
+})
+
+app.get('/courses', (req, res) => {
+  res.send('Hello! here are your courses')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
