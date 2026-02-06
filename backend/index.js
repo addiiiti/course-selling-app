0938ff00f2d67b1b35835a017252407726933dc0
addiiiti <aditiawez@gmail.com>
2026-02-06 16:30:38 +0000

    minor fixes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()//creates an instance of express app
 const port = 3000
 
-app.post("user/signup",(req,res)=>{
+app.post("/user/signup",(req,res)=>{
   res.json(
     {
       message: "signup endpoint"
@@ -10,29 +10,30 @@ app.post("user/signup",(req,res)=>{
   )
 })
 
-app.post("user/signin",(req,res)=>{
+app.post("/user/signin",(req,res)=>{
   res.json(
     {
       message: "signin endpoint"
     }
   )
 })
-app.post("user/purchases",(req,res)=>{
+app.get("/user/purchases",(req,res)=>{
   res.json(
     {
       message: "purchases endpoint"
     }
   )
 })
-app.post("user/purchase",(req,res)=>{
+app.post("/user/purchase",(req,res)=>{
   res.json(
     {
+      //user will have to pay for the course
       message: "purchase endpoint"
     }
   )
 })
 
-app.get('/courses', (req, res) => {
+app.get("/courses", (req, res) => {
   res.send('Hello! here are your courses')
 })
 
